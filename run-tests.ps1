3e4890bf765189a8b3e3597161050c4e23fd31e8
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2018-03-08 20:18:46 -0800

    Update run-tests.ps1

--- run-tests.ps1
+++ run-tests.ps1
@@ -1,1 +1,1 @@
-dotnet test unit-test-runners/DotNetCore-xUnit.csproj --configuration $env:platform --no-build -o "DotNetCore-xUnit.tests/bin/Any CPU/Debug/netcoreapp2.0/"
+dotnet test "C:\projects\unit-test-runners\unit-test-runners/DotNetCore-xUnit.csproj" --configuration $env:platform --no-build -o "DotNetCore-xUnit.tests/bin/Any CPU/Debug/netcoreapp2.0/"
